d61050cc675e61683ebdb1c2fabb76261ef3bd77
Ivan <ivanmolod@gmail.com>
2020-05-21 19:20:07 -0400

    testing other added files

--- sweetc.cpp
+++ sweetc.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "utils.h"
+#include "code_block.h"
+#include "fileRead.h"
 
 int main()
 {
+    auto lines = readFileLines("../test.spp");
+
+    for(auto& line : parseCodeBlock(lines)) {
+        std::cout << line << std::endl;
+    }
+
     return 0;
 }
