4df8ffbc9449327312fd92d9eabb152eea8e1ee2
Ivan <ivanmolod@gmail.com>
2020-05-23 10:30:28 -0400

    bug fixes and adding parseArguments function

--- argument.h
+++ argument.h
@@ -11,7 +11,13 @@ struct Argument
 
 std::ostream& operator<<(std::ostream& os, const Argument& argument)
 {
-    return os << argument.type << " " << argument.name << " = " << argument.defaultValue; 
+    os << argument.type << " " << argument.name;
+
+    if(!argument.defaultValue.empty()) {
+        os << " = " << argument.defaultValue;
+    }
+
+    return os;
 }
 
 Argument parseArgument(const std::string& str)
@@ -30,4 +36,17 @@ Argument parseArgument(const std::string& str)
     argument.type = parseType(line.substr(0, line.find_last_of(' ')));
 
     return argument;
+}
+
+std::vector<Argument> parseArguments(const std::string& str)
+{
+    std::vector<Argument> arguments;
+
+    std::string line = sanitize(str);
+
+    for(const std::string& argStr : smartSplit(line, ',', true)) {
+        arguments.push_back(parseArgument(argStr));
+    }
+
+    return arguments;
 }
