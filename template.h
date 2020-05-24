bf82b9d0dbc212acacc7d7cb00ab1e917054aae7
Ivan <ivanmolod@gmail.com>
2020-05-23 21:53:58 -0400

    creating function for parsing template

---
+++ template.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "utils.h"
+
+bool isTemplateArgument(const std::string& str)
+{
+    std::string line = sanitize(str);
+
+    return  line == "bool"
+    ||      line == "int"
+    ||      line == "unsigned"
+    ||      line == "long"
+    ||      line == "size_t";
+}
+
+std::vector<std::string> parseTemplates(const std::string& str)
+{
+    std::vector<std::string> templates;
+
+    auto split = smartSplit(sanitize(str), ',', true);
+
+    for(auto& s : split) {
+        s = sanitize(s);
+    }
+
+    if(split.size()>=2) {
+        for(auto& s : split) {
+            auto temps = parseTemplates(s);
+            templates.insert(templates.end(), temps.begin(), temps.end());
+        }
+    }
+    else
+    if(split[0].find('<')<split[0].length())
+    {
+        std::vector<std::string> templatesTemplates;
+
+        auto part = split[0].substr(split[0].find('<')+1);
+        part = sanitize(part.substr(0, part.find_last_of('>')));
+
+        for(auto& s : smartSplit(part, ',', true))
+        {
+            s = sanitize(s);
+
+            if(s != "class" && s != "typename" && !isTemplateArgument(split[0]))
+            {
+                auto temps = parseTemplates(s);
+                templates.insert(templates.end(), temps.begin(), temps.end());
+            }
+
+            if(s.find(' ') < s.find('<')) {
+                templatesTemplates.push_back(s.substr(0, s.find(' ')));
+            }
+            else {
+                templatesTemplates.push_back("class");
+            }
+        }
+
+        templates.push_back(split[0].substr(0, split[0].find('<'))+"<"+join(templatesTemplates,",")+">");
+    }
+    else
+    if(split[0].find_last_of(' ') < split[0].length())
+    {
+        templates.push_back(split[0]);
+    }
+    else
+    {
+        templates.push_back("class "+split[0]);
+    }
+
+    return templates;
+}
